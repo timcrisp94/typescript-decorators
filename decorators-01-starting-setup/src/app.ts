f8cac7bb872fcc16b6866cab97fc4c46f4c97c45
Tim Crisp <timothycrisp@gmail.com>
2022-05-09 10:40:14 -0500

    accessor and parameter decorators

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -38,11 +38,33 @@ function Log(target: any, propertyName: string) {
   console.log(target, propertyName)
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor){
+  console.log('accessor decorator')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('method decorator')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+  function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('parameter decorator')
+  console.log(target)
+  console.log(name)
+  console.log(position)
+}
+
 class Product {
   @Log
   title: string;
   private _price: number
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val
@@ -53,7 +75,8 @@ class Product {
     this._price = p;
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax)
   }
 }
