e78f07f8934adc34fa0cfd92ad90279163aa24bd
Tim Crisp <timothycrisp@gmail.com>
2022-05-09 10:13:52 -0500

    multiple decorators

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -1,11 +1,25 @@
 function Logger(logString: string) {
+  console.log('LOGGER FACTORY')
   return function(constructor: Function){
     console.log(logString)
     console.log(constructor)
   }
 }
 
-@Logger('LOGGING - PERSON')
+function WithTemplate(template: string, hookId: string) {
+  return function(constructor: any) {
+    console.log('TEMPLATE FACTORY')
+    const hookEl = document.getElementById(hookId)
+    const p = new constructor()
+    if (hookEl) {
+      hookEl.innerHTML = template
+      hookEl.querySelector('h1')!.textContent = p.name
+    }
+  }
+}
+
+@Logger('LOGGING')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name ='Max'
 
@@ -17,3 +31,4 @@ class Person {
 const per = new Person()
 
 console.log(per)
+
